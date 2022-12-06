4cb2e129bf477d5ad52d377356f4c405e0abeb6c
Asileka <asilkatya@gmail.com>
2022-12-06 23:49:43 +0000

    completed puzzle 1

---
+++ day1.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+function findFatElf (elvesDataFile) {
+ const elvesReadData = fs.readFile(elvesDataFile, (err, data) => {
+        let elvesData = data.toString().split("\n\n");
+        const elvesDataNewArray = [];
+      for (let i=0; i< elvesData.length; i++){
+        const separateElf = elvesData[i].split("\n").map(Number);
+        const separateElfTotalCal = separateElf.reduce((sum, a) => sum + a, 0)
+    elvesDataNewArray.push(separateElfTotalCal);
+      }
+      const fattestElf = Math.max(...elvesDataNewArray);
+       console.log(fattestElf);
+       return fattestElf;
+    });
+};
+const elvenTextDataFile = './elvesdata.txt'
+findFatElf(elvenTextDataFile);
