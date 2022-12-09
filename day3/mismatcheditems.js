a9a6142bce8e9ea62ccbefd8b67452d55cf93b32
Asileka <asilkatya@gmail.com>
2022-12-09 17:52:23 +0000

    solved day3 task 1

--- day3/mismatcheditems.js
+++ day3/mismatcheditems.js
@@ -5,17 +5,35 @@ function mismatchedItems(datafile) {
     const rucksacksArray2 = rucksacksArray.map(
       (x) => (x = [x.slice(0, x.length / 2), x.slice(x.length / 2)])
     );
-    //console.log(rucksacksArray2)
     const mismatchedItems = [];
-    for (let y = 0; y < rucksacksArray2[0][0].length; y++) {
-      for (let i = 0; i < rucksacksArray2[0][0].length; i++) {
-        if (rucksacksArray2[0][0][y] === rucksacksArray2[0][1][i]) {
-          mismatchedItems.push(rucksacksArray2[0][0][0]);
+    rucksacksArray2.map((y) => {
+      for (let i = 0; i < y[1].length; i++) {
+        if (y[1].includes(y[0][i])) {
+          mismatchedItems.push(y[0][i]);
+          i = y[1].length + 1;
         }
       }
-    }
-    console.log(rucksacksArray2[0][0][0]);
-    console.log(mismatchedItems);
+    });
+    // const alphabetLower = "abcdefghijklmnopqrstuvwxyz".split("");
+    // const alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    // const numbersArray1 = Array.from(Array(26).keys(), (x) => x + 1);
+    // const numbersArray2 = Array.from(Array(26).keys(), (x) => x + 27);
+    const alphabet =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    const numbersArray = Array.from(Array(52).keys(), (x) => x + 1);
+    let num = 1;
+    const lookupTable = alphabet.reduce((accumulator, value) => {
+      return {
+        ...accumulator,
+        [value]: num++,
+      };
+    }, {});
+    const convertedItems = mismatchedItems.map((key) => lookupTable[key]);
+    // console.log(convertedItems);
+    const sumOfPriorities = convertedItems.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    console.log(sumOfPriorities);
   });
 }
 const rucksackDataFile = "./rucksackdata.txt";
